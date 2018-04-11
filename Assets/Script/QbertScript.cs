a15af1fb97d38948946a22249b03d4dc506f190b
Diego  Camacho <DiegoDC97@gmail.com>
2018-04-10 20:07:22 -0400

    update

--- Assets/Script/QbertScript.cs
+++ Assets/Script/QbertScript.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QbertScript : MonoBehaviour {
+public class QbertScript : MonoBehaviour
+{
+
+    //GameObject Component References
+    Animation qBertAnimationComponent;
+    SpriteRenderer qbertSpriteRend;
+    Animator qbertAnim;
+
     //Current Cube
     NodeScript currentCube;
 
@@ -19,82 +26,93 @@ public class QbertScript : MonoBehaviour {
         }
     }
 
-    //Animator
-    Animator qbertAnim;
+    /// <summary>
+    /// QbertS Idle Sprites Array
+    /// </summary>
+    [SerializeField] Sprite[] QbertIdleSprites;
+    int movementDir = 0;
 
-	
+    bool moveNow = false;
+
 
     // Use this for initialization
-	void Start () {
+    void Start()
+    {
+        qBertAnimationComponent = GetComponent<Animation>();
+        qbertSpriteRend = GetComponent<SpriteRenderer>();
         currentCube = transform.parent.GetComponent<NodeScript>();
         qbertAnim = GetComponent<Animator>();
         if (CurrentCube != null)
         {
             Debug.Log(CurrentCube.name);
         }
-
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         InputManager();
-
+        if (moveNow)
+        {
+            Vector3 posOffset = new Vector3(CurrentCube.transform.position.x, CurrentCube.transform.position.y + 0.15f, 0);
+            transform.position = Vector2.MoveTowards(transform.position, posOffset, 0.05f);
+            if (transform.position == posOffset)
+            {
+                Debug.Log(movementDir);
+                moveNow = false;
+            }
+        }
     }
 
     void InputManager()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (CurrentCube.Adjacent[0] != null)
-            {
-                CurrentCube = CurrentCube.Adjacent[0];
-                transform.position = Vector3.MoveTowards(transform.position, CurrentCube.transform.position, 0.0002f);
-                qbertAnim.SetBool("Jump", true);
-                qbertAnim.SetInteger("Direction", 0);
-            }
+            QbertMove(CurrentCube.Adjacent[0], 0);
             Debug.Log("Q");
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
-            if (CurrentCube.Adjacent[1] != null)
-            {
-                CurrentCube = CurrentCube.Adjacent[1];
-                transform.position = Vector3.MoveTowards(transform.position, CurrentCube.transform.position, 0.0002f);
-                qbertAnim.SetBool("Jump", true);
-                qbertAnim.SetInteger("Direction", 1);
-            }
+            QbertMove(CurrentCube.Adjacent[1], 1);
             Debug.Log("W");
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            if (CurrentCube.Adjacent[2] != null)
-            {
-                CurrentCube = CurrentCube.Adjacent[2];
-                transform.position = Vector3.MoveTowards(transform.position, CurrentCube.transform.position, 0.0002f);
-                qbertAnim.SetBool("Jump", true);
-                qbertAnim.SetInteger("Direction", 2);
-            }
+            QbertMove(CurrentCube.Adjacent[2], 2);
             Debug.Log("A");
         }
-        else if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S)){
+            QbertMove(CurrentCube.Adjacent[3], 3);
+            Debug.Log("C");
+        }
+
+         if (Input.GetKeyDown(KeyCode.Escape))
+         {
+                Debug.Break();
+         }
+
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+            qbertSpriteRend.sprite = QbertIdleSprites[0];
+            Debug.Log(0);
+         }
+    }
+
+        void QbertMove(NodeScript destNode, int _dir)
         {
-            if (CurrentCube.Adjacent[3] != null)
+            if (currentCube)
             {
-                CurrentCube = CurrentCube.Adjacent[3];
-                transform.position = Vector3.MoveTowards(transform.position, CurrentCube.transform.position, 0.0002f);
+                CurrentCube = destNode;
+                movementDir = _dir;
+                moveNow = true;
+                //qBertAnimationComponent.Play();
                 qbertAnim.SetBool("Jump", true);
-                qbertAnim.SetInteger("Direction", 3);
+                qbertAnim.SetInteger("Direction", _dir);
             }
-            Debug.Log("S");
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        void DisableJump()
         {
-            Debug.Break();
+            qbertAnim.SetBool("Jump", false);
         }
-
-        ;
-
-	}
 }
